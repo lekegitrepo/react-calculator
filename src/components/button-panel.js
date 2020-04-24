ba05ac60eedbece950de76e392078b0fcb5c5421
lekegitrepo <lekedevrepo@gmail.com>
2020-04-24 15:51:14 -0500

    set the color and wide props for the button component

--- src/components/button-panel.js
+++ src/components/button-panel.js
@@ -5,32 +5,32 @@ import Button from './button';
 const ButtonPanel = () => (
   <div>
     <div className='row btn-group'>
-      <Button name='AC' btnColor='btn btn-medium' />
-      <Button name='+/-' btnColor='btn btn-medium' />
-      <Button name='%' btnColor='btn btn-medium' />
+      <Button name='AC' btnColor='btn btn-medium' color='grey' />
+      <Button name='+/-' btnColor='btn btn-medium' color='grey' />
+      <Button name='%' btnColor='btn btn-medium' color='grey' />
       <Button name='/' btnColor='btn btn-medium' />
     </div>
     <div className='row btn-group'>
-      <Button name='7' btnColor='btn btn-medium' />
-      <Button name='8' btnColor='btn btn-medium' />
-      <Button name='9' btnColor='btn btn-medium' />
+      <Button name='7' btnColor='btn btn-medium' color='grey' />
+      <Button name='8' btnColor='btn btn-medium' color='grey' />
+      <Button name='9' btnColor='btn btn-medium' color='grey' />
       <Button name='X' btnColor='btn btn-medium' />
     </div>
     <div className='row btn-group'>
-      <Button name='4' btnColor='btn btn-medium' />
-      <Button name='5' btnColor='btn btn-medium' />
-      <Button name='6' btnColor='btn btn-medium' />
+      <Button name='4' btnColor='btn btn-medium' color='grey' />
+      <Button name='5' btnColor='btn btn-medium' color='grey' />
+      <Button name='6' btnColor='btn btn-medium' color='grey' />
       <Button name='-' btnColor='btn btn-medium' />
     </div>
     <div className='row btn-group'>
-      <Button name='1' btnColor='btn btn-medium' />
-      <Button name='2' btnColor='btn btn-medium' />
-      <Button name='3' btnColor='btn btn-medium' />
+      <Button name='1' btnColor='btn btn-medium' color='grey'/>
+      <Button name='2' btnColor='btn btn-medium' color='grey'/>
+      <Button name='3' btnColor='btn btn-medium' color='grey'/>
       <Button name='+' btnColor='btn btn-medium' />
     </div>
     <div className='row btn-group'>
-      <Button name='0' btnColor='btn btn-large' />
-      <Button name='.' btnColor='btn btn-medium' />
+      <Button name='0' btnColor='btn btn-large' wide color='grey'/>
+      <Button name='.' btnColor='btn btn-medium' color='grey' />
       <Button name='=' btnColor='btn btn-medium' />
     </div>
   </div>
--- src/components/button.js
+++ src/components/button.js
@@ -2,12 +2,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name, btnColor }) => (
-  <button type="button" className={ btnColor } >
-    { name }
-  </button>
-);
+const Button = ({ name, btnColor, color, wide }) => {
+  const style = {
+    width: wide ? '50%' : '25%',
+    backgroundColor: color,
+  };
+  return (
+    <button type="button" className={ btnColor } style={style} >
+      { name }
+    </button>
+  )
+};
 
-Button.propTypes = { name: PropTypes.string.isRequired };
+Button.propTypes = { 
+  name: PropTypes.string.isRequired,
+  wide: PropTypes.bool
+};
+
+Button.defaultProps = {
+  wide: false,
+  color: 'orange',
+};
 
 export default Button;
