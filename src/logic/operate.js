728eb52ae0e7bcb785fd2ada432c6cc9630a20f5
lekegitrepo <lekedevrepo@gmail.com>
2020-05-13 15:20:03 -0500

    add try and catch to the operate switch block

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -6,20 +6,24 @@ const operate = (numberOne, numberTwo, operation) => {
   const operandOne = Big(numberOne);
   const operandTwo = Big(numberTwo);
 
-  switch (operation) {
-    case 'X':
-      return operandOne.times(operandTwo).toString();
-    case '-':
-      return operandOne.minus(operandTwo).toString();
-    case '+':
-      return operandOne.plus(operandTwo).toString();
-    case '%':
-      return operandTwo.div('100').toString();
-    case '÷':
-      if (operandTwo === 0) return "Error: You can't divide by 0";
-      return operandOne.div(operandTwo).toString();
-    default:
-      return 0;
+try {
+    switch (operation) {
+      case 'X':
+        return operandOne.times(operandTwo).toString();
+      case '-':
+        return operandOne.minus(operandTwo).toString();
+      case '+':
+        return operandOne.plus(operandTwo).toString();
+      case '%':
+        return operandTwo.div('100').toString();
+      case '÷':
+        if (operandTwo === 0) return "Error: You can't divide by 0";
+        return operandOne.div(operandTwo).toString();
+      default:
+        return 0;
+    }
+  }catch(err) {
+    Error(`Error: incorrect input! ${err}`);
   }
 };
 
